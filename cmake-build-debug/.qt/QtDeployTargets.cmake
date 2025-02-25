aed546a1c0d541c8d5c39136dc08afc9019d7ddb
JellyfishKngiht <1002984726@qq.com>
2025-02-25 22:01:42 +0800

    remove build

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Babble_FILE D:/Babble/cmake-build-debug/Babble.exe)
-set(__QT_DEPLOY_TARGET_Babble_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Babble_RUNTIME_DLLS D:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
