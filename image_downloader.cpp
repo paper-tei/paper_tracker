36d31bbda12f9de4c3d736509775dba58f5c69e3
JellyfishKngiht <1002984726@qq.com>
2025-03-02 18:34:19 +0800

    Access violation reading location 0x0000000d

--- image_downloader.cpp
+++ image_downloader.cpp
@@ -4,136 +4,15 @@
 #include "image_downloader.hpp"
 
 #include <iostream>
+#include <curl/curl.h>
 #include <opencv2/imgcodecs.hpp>
 
 
-ImageDownloader::ImageDownloader(const std::string& ip, const std::string& port) :
-    ip(ip), port(port), thread_pool_(4)
-{
-    WSADATA wsaData;
-    int iResult;
-
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        std::cerr << "WSAStartup failed: " << iResult << std::endl;
-        return;
-    }
-
-    // Create a SOCKET for the server to listen for client connections
-    listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listenSocket == INVALID_SOCKET) {
-        std::cerr << "socket failed with error: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return;
-    }
-
-    // The sockaddr structure specifies the address family,
-    // IP address, and port for the socket that is being bound
-    sockaddr_in service{};
-    service.sin_family = AF_INET;
-    service.sin_addr.s_addr = inet_addr(ip.c_str());
-    service.sin_port = htons(std::stoi(port));
-
-    // Bind the socket
-    if (bind(listenSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-        std::cerr << "bind failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(listenSocket);
-        WSACleanup();
-        return;
-    }
-
-    // Listen for incoming connection requests
-    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cerr << "listen failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(listenSocket);
-        WSACleanup();
-        return;
-    }
-}
+ImageDownloader::ImageDownloader() : thread_pool_(4) {}
 
 ImageDownloader::~ImageDownloader()
 {
-    stop_video_stream_server();
     thread_pool_.stop_now();
-}
-
-void ImageDownloader::stop_video_stream_server()
-{
-    server_running = false;
-}
-
-void ImageDownloader::client_handler(SOCKET client_socket)
-{
-    char buffer[1024];
-    int bytesReceived = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-
-    if (bytesReceived <= 0) {
-        closesocket(client_socket);
-        return;
-    }
-
-    std::string responseHeader =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: multipart/x-mixed-replace;boundary=" + BOUNDARY + "\r\n"
-        "Cache-Control: no-cache\r\n"
-        "Connection: close\r\n"
-        "Access-Control-Allow-Origin: *\r\n"
-        "\r\n";
-
-    send(client_socket, responseHeader.c_str(), static_cast<int>(responseHeader.length()), 0);
-    while (server_running) {
-        cv::Mat frame = this->get_video_frame();
-        if (frame.empty()) {
-            continue;
-        }
-
-        std::vector<uchar> frame_buffer;
-        cv::imencode(".mjpeg", frame, frame_buffer);
-
-        std::string frameHeader =
-            "--" + BOUNDARY + "\r\n"
-            "Content-Type: image/jpeg\r\n"
-            "Content-Length: " + std::to_string(frame_buffer.size()) + "\r\n"
-            "\r\n";
-
-        int result = send(client_socket, frameHeader.c_str(), static_cast<int>(frameHeader.length()), 0);
-        if (result == SOCKET_ERROR) break;
-
-        result = send(client_socket, reinterpret_cast<const char*>(frame_buffer.data()), static_cast<int>(frame_buffer.size()), 0);
-        if (result == SOCKET_ERROR) break;
-
-        std::string frameFooter = "\r\n";
-        result = send(client_socket, frameFooter.c_str(), static_cast<int>(frameFooter.length()), 0);
-        if (result == SOCKET_ERROR) break;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-    closesocket(client_socket);
-}
-
-bool ImageDownloader::start_video_stream_server()
-{
-    server_running = true;
-    thread_pool_.submit([this] ()
-    {
-        while (server_running)
-        {
-            sockaddr_in clientAddr{};
-            int addrLen = sizeof(clientAddr);
-            SOCKET clientSocket = accept(listenSocket, reinterpret_cast<sockaddr*>(&clientAddr), &addrLen);
-
-            if (clientSocket != INVALID_SOCKET) {
-                thread_pool_.submit([clientSocket, this] ()
-                {
-                    client_handler(clientSocket);
-                });
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-    });
-
-    return true;
 }
 
 cv::Mat ImageDownloader::get_video_frame()
@@ -147,12 +26,11 @@ cv::Mat ImageDownloader::get_video_frame()
     return frame;
 }
 
-void ImageDownloader::start_video_from_esp32(const std::string& url, std::vector<char>& buffer)
+void ImageDownloader::start_video_from_esp32(const std::string& url, std::vector<char>* buffer)
 {
     int maxRetries = 30;
     int retryCount = 0;
     CURL* curl = nullptr;
-    CURLcode res;
 
     if (retryCount < maxRetries) {
         curl = curl_easy_init();
@@ -160,24 +38,22 @@ void ImageDownloader::start_video_from_esp32(const std::string& url, std::vector
             curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &ImageDownloader::write_callback);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void*>(buffer));
             curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 1L);
             curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 3000L);
 
-            res = curl_easy_perform(curl);
+            const auto res = curl_easy_perform(curl);
 
             if (res == CURLE_OK) {
                 std::cout << "CURL request successful" << std::endl;
                 curl_easy_cleanup(curl);
                 return;
-            }
-            else {
+            } else {
                 std::cerr << "CURL request failed: " << curl_easy_strerror(res) << std::endl;
                 retryCount++;
                 std::cout << "Retrying (" << retryCount << "/" << maxRetries << ")..." << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));  // ??? 1 ???????
             }
-
             curl_easy_cleanup(curl);
         }
         else {
@@ -185,7 +61,7 @@ void ImageDownloader::start_video_from_esp32(const std::string& url, std::vector
         }
     }
 
-    if (retryCount == maxRetries) {
+    if (retryCount >= maxRetries) {
         std::cerr << "Failed to download image after " << maxRetries << " retries." << std::endl;
     }
 }
@@ -197,37 +73,39 @@ void ImageDownloader::update_video_frame(cv::Mat frame)
 
 size_t ImageDownloader::write_callback(void* contents, size_t size, size_t nmemb, void* userp)
 {
-    static std::vector<char> residualBuffer; // ????????????
+    static std::vector<char> residualBuffer;
     const size_t totalSize = size * nmemb;
-    std::vector<char> buffer = *static_cast<std::vector<char>*>(userp);
+    std::vector<char>* buffer = static_cast<std::vector<char>*>(userp);
+    std::cout << buffer->size();
 
-    // ???????????
     if (!residualBuffer.empty()) {
-        buffer.insert(buffer.begin(), residualBuffer.begin(), residualBuffer.end());
+        buffer->insert(buffer->begin(), residualBuffer.begin(), residualBuffer.end());
         residualBuffer.clear();
     }
 
-    // ?????????????????
     {
         std::lock_guard<std::mutex> lock(bufferMutex);
-        buffer.insert(buffer.end(),
+        buffer->insert(buffer->end(),
             static_cast<char*>(contents),
             static_cast<char*>(contents) + totalSize);
     }
 
     while (true) {
-        auto boundaryStart = std::ranges::search(buffer, BOUNDARY_HEADER).begin();
+        auto boundaryStart = std::search(
+            buffer->begin(), buffer->end(),
+            BOUNDARY_HEADER.begin(), BOUNDARY_HEADER.end()
+        );
 
-        if (boundaryStart == buffer.end()) break;
+        if (boundaryStart == buffer->end()) break;
 
         auto boundaryEnd = std::search(
-            boundaryStart + BOUNDARY_HEADER.size(), buffer.end(),
+            boundaryStart + BOUNDARY_HEADER.size(), buffer->end(),
             BOUNDARY_END.begin(), BOUNDARY_END.end()
         );
 
-        if (boundaryEnd == buffer.end()) {
-            residualBuffer.assign(boundaryStart, buffer.end());
-            buffer.clear();
+        if (boundaryEnd == buffer->end()) {
+            residualBuffer.assign(boundaryStart, buffer->end());
+            buffer->clear();
             break;
         }
 
@@ -254,7 +132,7 @@ size_t ImageDownloader::write_callback(void* contents, size_t size, size_t nmemb
         }
 
         // ?????????
-        buffer.erase(buffer.begin(), boundaryEnd + BOUNDARY_END.size());
+        buffer->erase(buffer->begin(), boundaryEnd + BOUNDARY_END.size());
     }
 
     return totalSize;
--- include/image_downloader.hpp
+++ include/image_downloader.hpp
@@ -6,15 +6,8 @@
 #define IMAGE_DOWNLOADER_HPP
 #include <string>
 #include <chrono>
-#include <thread>
-#include <curl/curl.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
-#include <iostream>
 #include <vector>
 #include <mutex>
-#include <atomic>
 #include <opencv2/core.hpp>
 
 #include "thread_pool.hpp"
@@ -25,35 +18,20 @@ const std::string BOUNDARY_END = "--123456789000000000000987654321";
 
 class ImageDownloader {
 public:
-    ImageDownloader(const std::string& ip, const std::string& port);
+    ImageDownloader();
 
     ~ImageDownloader();
 
-    void start_video_from_esp32(const std::string& url, std::vector<char>& buffer);
+    void start_video_from_esp32(const std::string& url, std::vector<char>* buffer);
 
-    // underwear function. Author: paper-tei
-    bool start_video_stream_server();
-    // underwear function. Author: paper-tei
-    void stop_video_stream_server();
-private:
     cv::Mat get_video_frame();
-
+private:
     void update_video_frame(cv::Mat frame);
 
     size_t write_callback(void* contents, size_t size, size_t nmemb, void* userp);
 
-    // underwear function. Author: paper-tei
-    void client_handler(SOCKET client_socket);
-
-    SOCKET listenSocket;
-    std::string ip;
-    std::string port;
     utils::ThreadPool thread_pool_;
-
-    bool server_running = false;
-
     std::queue<cv::Mat> video_frame_queue_;
-
     std::mutex bufferMutex;
     std::vector<char> imageBuffer;
 };
