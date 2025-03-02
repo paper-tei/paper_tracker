14d88dc33dba5939e92e48efd7cebcc955dde553
JellyfishKngiht <1002984726@qq.com>
2025-03-02 15:25:05 +0800

    add curl lib

--- include/image_downloader.hpp
+++ include/image_downloader.hpp
@@ -1,31 +1,31 @@
+// //
+// // Created by JellyfishKnight on 25-3-2.
+// //
 //
-// Created by JellyfishKnight on 25-3-2.
+// #ifndef IMAGE_DOWNLOADER_HPP
+// #define IMAGE_DOWNLOADER_HPP
+// #include <string>
+// #include <chrono>
+// #include <thread>
+// #include <curl/curl.h>
 //
-
-#ifndef IMAGE_DOWNLOADER_HPP
-#define IMAGE_DOWNLOADER_HPP
-#include <string>
-#include <chrono>
-#include <thread>
-
-
-
-class ImageDownloader {
-public:
-    ImageDownloader() = default;
-    ~ImageDownloader() = default;
-
-    void download_image(const std::string& url, const std::string& save_path);
-
-    bool start_video_stream_server(int port = 8080);
-
-    void stop_video_stream_server();
-private:
-    void write_callback(void* contents, size_t size, size_t nmemb, void* userp);
-
-};
-
-
-
-
-#endif //IMAGE_DOWNLOADER_HPP
+//
+// class ImageDownloader {
+// public:
+//     ImageDownloader() = default;
+//     ~ImageDownloader() = default;
+//
+//     void download_image(const std::string& url, const std::string& save_path);
+//
+//     bool start_video_stream_server(int port = 8080);
+//
+//     void stop_video_stream_server();
+// private:
+//     void write_callback(void* contents, size_t size, size_t nmemb, void* userp);
+//
+// };
+//
+//
+//
+//
+// #endif //IMAGE_DOWNLOADER_HPP
--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include "logger.h"
 #include "main_window.hpp"
 
+#include <curl/curl.h>
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    curl_easy_init();
 
     PaperTrackMainWindow window;
     window.show();
